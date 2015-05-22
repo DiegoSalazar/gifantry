f62632543b1105664f876e517f0026015267c045
Prelang Builder <builder@prelang.com>
2015-05-22 18:16:22 +0000

    Setting the User of the model on #create to the currently signed in user

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -29,6 +29,7 @@ class AlbumsController < ApplicationController
   # POST /albums.json
   def create
     @album = Album.new(album_params)
+    @album.user = current_user
 
     respond_to do |format|
       if @album.save
