d8b3c1ec83faaa0522b8214e339cb2887ca3b6de
Prelang Builder <builder@prelang.com>
2015-05-22 18:16:22 +0000

    Generated scaffolding for model 'Album' via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :albums
+
   resources :entries
 
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
