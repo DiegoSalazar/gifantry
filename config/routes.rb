df394286edb135bda7ec0a400afaa904d6541b66
Diego Salazar <diego@greyrobot.com>
2015-05-27 10:34:36 -0400

    turned off sign up

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
     post   "login"   => "devise/sessions#create",      as: :user_session
     delete "signout" => "devise/sessions#destroy",     as: :destroy_user_session
     
-    get    "signup"  => "devise/registrations#new",    as: :new_user_registration
-    post   "signup"  => "devise/registrations#create", as: :user_registration
-    put    "signup"  => "devise/registrations#update", as: :update_user_registration
+    # get    "signup"  => "devise/registrations#new",    as: :new_user_registration
+    # post   "signup"  => "devise/registrations#create", as: :user_registration
+    # put    "signup"  => "devise/registrations#update", as: :update_user_registration
     get    "account" => "devise/registrations#edit",   as: :edit_user_registration
   end
 end
